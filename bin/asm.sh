821a0e8ae417e39539215edd5849989044025a87
root <andre>
2017-04-14 19:55:56 +0800

    add Makefile

--- bin/asm.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-cd src
-nasm helloos.asm -o greenos.img
-mv greenos.img ../bin/
--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,6 @@
-cd bin
+#! /bin/sh
+
+cd ../src
+make 
+echo "greenos.img is ready"
 qemu-system-i386 -drive file=greenos.img,format=raw,index=0,media=disk
